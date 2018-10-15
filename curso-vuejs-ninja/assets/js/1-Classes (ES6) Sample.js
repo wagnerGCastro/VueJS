9e597f035ee887b406810d66432f7d4640cf858b
wagner <wagner@futmanager.com>
2018-10-15 07:31:53 -0300

    'componentsvuejs2'

---
+++ curso-vuejs-ninja/assets/js/1-Classes (ES6) Sample.js
@@ -0,0 +1,19 @@
+﻿window.onload = function () {
+    sepExpressoes();
+    
+}
+
+
+
+function sepExpressoes() {
+    var idade = 6, sexo = "masculino", nac = "brasileiro";
+    var x = (idade >= 18) ? "Maior de Idade" : sexo;
+    alert(x);
+}
+
+$(document).ready(function () {
+    alert("slide_Jquery");
+    
+    $( "#tabs" ).tabs();
+
+});
